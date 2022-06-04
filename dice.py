20acc7699fe909ee937350fd1ae3f3426af57281
i6nx! - <ignacio.logiudice@gmail.com>
2022-06-03 22:05:57 -0300

    Initial commit

---
+++ dice.py
@@ -0,0 +1,21 @@
+def parse_input(input_string):
+    """
+    Check if `input_string` is an integer number between 1-6.
+    If so, return an integer with the same value.
+    Else, warn user and quit the program.
+    """
+    if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
+        return int(input_string)
+    else:
+        print("Please enter a number from 1 to 6.")
+        raise SystemExit(1)
+
+
+
+
+######################### MAIN ######################### 
+
+# GET INPUT
+num_input = input("Write the amount of dices to be rolled: [1-6]")
+num = parse_input(num_input)
+
